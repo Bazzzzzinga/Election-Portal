5ff9a1f2871e7bff0ba5eea87b2a6419b6650a3a
Pranjal Jain <pranjal.jain.cse15@iitbhu.ac.in>
2016-09-14 04:30:57 +0530

    template added

--- Election_Portal/urls.py
+++ Election_Portal/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
 app_name='Election_Portal'
-urlpatterns=[]
+urlpatterns=[url(r'^$',views.index,name='index'),]
