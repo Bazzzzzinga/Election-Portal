d46def2cf4ed0f89ed198ff96ba888fbcf7304b4
Pranjal Jain <pranjal.jain.cse15@iitbhu.ac.in>
2016-09-15 01:34:20 +0530

    Bootstrap->Materialize

--- Election_Portal/urls.py
+++ Election_Portal/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
 app_name='Election_Portal'
-urlpatterns=[url(r'^$',views.index,name='index'),]
+urlpatterns=[url(r'^$',views.index,name='index'),
+			url(r'^home/$',views.home,name='home'),
+]
--- Election_Portal/views.py
+++ Election_Portal/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,"Election_Portal/index.html")
+def home(request):
+	return render(request,"Election_Portal/home.html")
