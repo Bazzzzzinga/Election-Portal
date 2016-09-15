619526adefc34f64253d60a4d9670ad472d55f6e
Vinay Jaisinghani <vinayjaisinghani7777@gmail.com>
2016-09-16 03:45:01 +0530

    ongoing, nominations, status added, ~files added to gitignore

--- Election_Portal/urls.py
+++ Election_Portal/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name='Election_Portal'
 urlpatterns=[url(r'^$',views.index,name='index'),
 			url(r'^home/$',views.home,name='home'),
+			url(r'^OnGoing/$',views.OnGoing,name='OnGoing'),
+		        url(r'^nominations/$',views.nominations,name='nominations'),
+			url(r'^status/$',views.status,name='status'),
+			url(r'^past/$',views.past,name='past'),
+			url(r'^ama/$',views.ama,name='ama')
 ]
--- Election_Portal/views.py
+++ Election_Portal/views.py
@@ -5,3 +5,13 @@ def index(request):
 	return render(request,"Election_Portal/index.html")
 def home(request):
 	return render(request,"Election_Portal/home.html")
+def OnGoing(request):
+	return render(request,"Election_Portal/OnGoing.html")
+def nominations(request):
+	return render(request,"Election_Portal/nominations.html")
+def status(request):
+	return render(request,"Election_Portal/status.html")
+def past(request):
+	return render(request,"Election_Portal/past.html")
+def ama(request):
+	return render(request,"Election_Portal/ama.html")
