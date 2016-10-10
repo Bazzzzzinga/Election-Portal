ff917a8d52b66e8b0b723565c537052b7e6a4551
Pranjal Jain <pranjal.jain.cse15@iitbhu.ac.in>
2016-10-11 03:23:26 +0530

    Template fixed

--- Election_Portal/urls.py
+++ Election_Portal/urls.py
@@ -5,7 +5,7 @@ app_name='Election_Portal'
 urlpatterns=[url(r'^$',views.index,name='index'),
 			url(r'^home/$',views.home,name='home'),
 			url(r'^OnGoing/$',views.OnGoing,name='OnGoing'),
-		        url(r'^nominations/$',views.nominations,name='nominations'),
+		    url(r'^nominations/$',views.nominations,name='nominations'),
 			url(r'^status/$',views.status,name='status'),
 			url(r'^past/$',views.past,name='past'),
 			url(r'^ama/$',views.ama,name='ama'),
